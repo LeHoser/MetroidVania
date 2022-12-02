706ea5ddff0bdbdb9e7fa6a0d02c493322c3efba
Nathan Lloyd <nathan.lloyd.7003@gmail.com>
2022-12-02 10:37:20 -0400

    Added animation for firing. Added coyote time to jump and jump buffer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private Transform _groundCheck;
 
-    private bool _isGrounded;
+    [SerializeField] private bool _isGrounded;
+    [SerializeField] private bool _isJumping;
 
     public LayerMask whatIsGround;
 
@@ -19,8 +20,13 @@ public class PlayerController : MonoBehaviour
     public BulletController shotToFire;
     public Transform shotPoint;
 
+    [SerializeField] private bool _canDoubleJump;
+
     [SerializeField] private float _coyoteTime = 0.2f;
-    private float _coyoteTimeCounter;
+    [SerializeField] private float _coyoteTimeCounter;
+
+    [SerializeField] private float _jumpBufferTime = 0.2f;
+    [SerializeField] private float _jumpBufferCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -49,7 +55,16 @@ public class PlayerController : MonoBehaviour
             _coyoteTimeCounter -= Time.deltaTime;
         }
 
-        if(_rb.velocity.x < 0)
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jumpBufferCounter = _jumpBufferTime;
+        }
+        else
+        {
+            _jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (_rb.velocity.x < 0)
         {
             transform.localScale = new Vector3(-1, 1f, 1f);
         }
@@ -60,11 +75,21 @@ public class PlayerController : MonoBehaviour
 
         _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, .2f, whatIsGround);
 
-        if(Input.GetButtonDown("Jump") && _coyoteTimeCounter > 0f)
+        if(_coyoteTimeCounter > 0f && _jumpBufferCounter > 0f && !_isJumping)
         {
+            //_canDoubleJump = true;
             _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
-        }
+            if(_isJumping == true)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+                _canDoubleJump = false;
+            }
 
+            _jumpBufferCounter = 0f;
+
+            StartCoroutine(JumpCooldown());
+        }
+        
         anim.SetBool("IsGrounded", _isGrounded);
         anim.SetFloat("speed", Mathf.Abs(_rb.velocity.x));
     }
@@ -74,6 +99,14 @@ public class PlayerController : MonoBehaviour
         if(Input.GetButtonDown("Fire1"))
         {
             Instantiate(shotToFire, shotPoint.position, shotPoint.rotation)._moveDir = new Vector2(transform.localScale.x, 0);
+
+            anim.SetTrigger("shotFired");
         }
+    }
+    private IEnumerator JumpCooldown()
+    {
+        _isJumping = true;
+        yield return new WaitForSeconds(0.4f);
+        _isJumping = false;
     }
 }
